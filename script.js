23583c68b0911e4466b5397930a73f917adca3b7
Adrich-Fernandes <adrichancyfernandes@gmail.com>
2025-10-15 17:10:30 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const menu = document.querySelector('.menu')
-const phone = document.querySelector('.phone')
-const close = document.querySelector('.close')
-
-menu.addEventListener('click',function(){
-  phone.style.display = "flex"
-})
-
-close.addEventListener('click',function(){
-  phone.style.display = "none"
-})
+const menu = document.querySelector('.menu');
+let pos = document.querySelector('.pos');
+let active = true;
+menu.addEventListener('click',()=>{
+ 
+  if(active){
+    pos.style.display = 'flex';
+    active = false;
+  }
+  else{
+    pos.style.display = 'none';
+    active = true;
+  }
+});
