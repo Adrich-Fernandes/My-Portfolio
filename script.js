a416c651850acb47f85f311158de1f97dafe100f
Adrich-Fernandes <adrichancyfernandes@gmail.com>
2025-10-29 21:13:35 +0530

    updated

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-// access the element and assigne it to a variable...>
-const java = document.querySelector('.java');
-const js = document.querySelector('.js');
-const css = document.querySelector('.css');
-const py = document.querySelector('.python');
-const re = document.querySelector('.react');
-const html = document.querySelector('.html');
-const simg = document.querySelector('.simg');
-
 const menu = document.querySelector('.menu');
 let pos = document.querySelector('.pos');
 let active = true;
 
+
+
+const navp = document.querySelectorAll('.pon-nav').forEach(items=>{
+  items.addEventListener('click',()=>{
+    menu.textContent = "menu"
+    pos.style.display = 'none';
+    active = true;
+  });
+})
 // to open the menu for more options...>
 menu.addEventListener('click',()=>{
-
   // open the menu...>
   if(active){
     menu.textContent = "close"
@@ -26,8 +25,4 @@ menu.addEventListener('click',()=>{
     pos.style.display = 'none';
     active = true;
   }
-});
-
-simg.addEventListener('mouseover',()=>{
-  
-});
+}); 
